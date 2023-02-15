709756c6a5ecbb1cbc0814616c0c601e4a6ec9e8
rs-rahamn <jesminakter.rahaman@gmail.com>
2023-02-15 22:13:03 +0600

    this is goriber text editro

---
+++ calculator.js
@@ -0,0 +1,70 @@
+// bold
+document.getElementById("bold-btn").addEventListener("click", function () {
+  let getInputField = document.getElementById("input-field");
+  let styleInputField =
+    ((getInputField.style.fontWeight = "bold"),
+    (getInputField.style.fontSize = "15px"));
+  getInputField = styleInputField;
+});
+
+// italic
+document.getElementById("italic-btn").addEventListener("click", function () {
+  let getInputField = document.getElementById("input-field");
+  let styleInputField =
+    ((getInputField.style.fontStyle = "italic"),
+    (getInputField.style.fontSize = "15px"));
+  getInputField = styleInputField;
+});
+
+// underline-btn
+document.getElementById("underline-btn").addEventListener("click", function () {
+  let getInputField = document.getElementById("input-field");
+  let styleInputField =
+    ((getInputField.style.textDecoration = "underline"),
+    (getInputField.style.fontSize = "15px"));
+  getInputField = styleInputField;
+});
+
+// left
+document.getElementById("left-btn").addEventListener("click", function () {
+  let getInputField = document.getElementById("input-field");
+  let styleInputField =
+    ((getInputField.style.textAlign = "left"),
+    (getInputField.style.fontSize = "15px"));
+  getInputField = styleInputField;
+});
+// right
+document.getElementById("right-btn").addEventListener("click", function () {
+  let getInputField = document.getElementById("input-field");
+  let styleInputField =
+    ((getInputField.style.textAlign = "right"),
+    (getInputField.style.fontSize = "15px"));
+  getInputField = styleInputField;
+});
+
+// justify
+document.getElementById("justify-btn").addEventListener("click", function () {
+  let getInputField = document.getElementById("input-field");
+  let styleInputField =
+    ((getInputField.style.textAlign = "justify"),
+    (getInputField.style.fontSize = "15px"));
+  getInputField = styleInputField;
+});
+
+// center
+document.getElementById("center-btn").addEventListener("click", function () {
+  let getInputField = document.getElementById("input-field");
+  let styleInputField =
+    ((getInputField.style.textAlign = "center"),
+    (getInputField.style.fontSize = "15px"));
+  getInputField = styleInputField;
+});
+
+// color
+/* document.getElementById("inputColor").addEventListener("click", function () {
+  let getInputField = document.getElementById("input-field");
+  let getInputFieldValue = getInputField.value;
+  let styleGetInputFieldValue =
+    ((getInputField.style.color = "red"),
+    (getInputField.style.fontSize = "15px"));
+}) */
